4325aadc352c5fd48e4f06ef5e6b93f231891c07
Cyril <cyril.grelier@outlook.fr>
2024-07-17 16:50:15 +0200

    update drawing functions and use numpy 1.26.4 (error with 2.0.0)

--- evomol/visualization/draw_mol.py
+++ evomol/visualization/draw_mol.py
@@ -1,67 +1,85 @@
-import io
-import os
-
-from PIL import Image
 from rdkit.Chem import Draw
-from rdkit.Chem.rdDepictor import Compute2DCoords
+from IPython.display import SVG, display
+import matplotlib.pyplot as plt
+from io import BytesIO
+from PIL import Image
+import cairosvg
 
 from evomol.representation import MolecularGraph, Molecule
 
 
-def draw(
+def mol_to_svg(
     molecule: Molecule,
-    show: bool = True,
-    size: int = 200,
-    write_to_path: str = "",
-) -> None:
-    """
-    Drawing the molecule
-    """
-
+    size=500,
+    draw_index=True,
+    notes_on_atoms: dict[int, str] = None,
+    notes_on_bonds: dict[int, str] = None,
+) -> str:
     mol = molecule.get_representation(MolecularGraph).mol.GetMol()
 
-    # Computing coordinates and making sure the properties are computed
-    Compute2DCoords(mol)
-    mol.UpdatePropertyCache()
-
-    # Drawing the molecule
-    dr = Draw.rdMolDraw2D.MolDraw2DCairo(size, size)
-    opts = dr.drawOptions()
-
-    # Transparent background if not writing to file
-    if not write_to_path:
-        opts.clearBackground = False
-
-    dr.DrawMolecule(mol)
-    dr.FinishDrawing()
-
-    # Loading the molecule as a PIL object
-    bytes_images = dr.GetDrawingText()
-    image = Image.open(io.BytesIO(bytes_images))
-
-    if show:
-        image.show()
-
-    if write_to_path:
-        # Creating directories if they don't exist
-        os.makedirs(os.path.dirname(write_to_path), exist_ok=True)
-
-        # Writing image to disk
-        image.save(write_to_path, "PNG")
-
-    # return image
-
-
-def draw_with_index(molecule: Molecule) -> None:
-
-    mol = molecule.get_representation(MolecularGraph).mol.GetMol()
-
-    d = Draw.rdMolDraw2D.MolDraw2DCairo(1000, 1000)  # or MolDraw2DSVG to get SVGs
+    # other : Draw.rdMolDraw2D.MolDraw2DCairo
+    d = Draw.rdMolDraw2D.MolDraw2DSVG(size, size)
     d.drawOptions().addStereoAnnotation = True
-    d.drawOptions().addAtomIndices = True
+    if draw_index:
+        d.drawOptions().addAtomIndices = True
+
+    if notes_on_atoms is not None:
+        for atom_idx, note in notes_on_atoms.items():
+            mol.GetAtomWithIdx(atom_idx).SetProp("atomNote", note)
+    if notes_on_bonds is not None:
+        for bond_idx, note in notes_on_bonds.items():
+            mol.GetBondWithIdx(bond_idx).SetProp("bondNote", note)
+
     d.DrawMolecule(mol)
     d.FinishDrawing()
-    d.WriteDrawingText(f"{molecule}.png")
 
-    # mol.GetAtomWithIdx(2).SetProp("atomNote", "foo")
-    # mol.GetBondWithIdx(0).SetProp("bondNote", "bar")
+    svg = d.GetDrawingText()
+
+    return svg
+
+
+def draw_in_jupyter(svg: str) -> None:
+    display(SVG(svg))
+
+
+def draw_in_matplotlib(svg: str) -> None:
+    # convert SVG to PNG using cairosvg
+    png_data = cairosvg.svg2png(bytestring=svg.encode("utf-8"))
+
+    # load the PNG image with PIL
+    image = Image.open(BytesIO(png_data))
+
+    # create a matplotlib figure and display the image
+    fig, ax = plt.subplots(figsize=(image.width / 100, image.height / 100))
+    ax.imshow(image)
+    ax.axis("off")
+    plt.show()
+
+
+def draw_multiple_svgs_in_matplotlib(svgs: list) -> None:
+    # convert each SVG to PNG using cairosvg
+    png_datas = [
+        Image.open(BytesIO(cairosvg.svg2png(bytestring=svg.encode("utf-8"))))
+        for svg in svgs
+    ]
+
+    # set the number of columns and rows for the subplots
+    num_images = len(png_datas)
+    # 3 columns maximum
+    cols = min(3, num_images)
+    rows = (num_images + cols - 1) // cols
+
+    # create a figure with subplots
+    fig, axes = plt.subplots(rows, cols, figsize=(cols * 5, rows * 5))
+
+    # display each image in a subplot
+    for i, (image, ax) in enumerate(zip(png_datas, axes.flat)):
+        ax.imshow(image)
+        ax.axis("off")
+
+    # remove the axis for the empty subplots
+    for j in range(i + 1, len(axes.flat)):
+        axes.flat[j].axis("off")
+
+    plt.tight_layout()
+    plt.show()
--- scripts/draw_mol.py
+++ scripts/draw_mol.py
@@ -1,10 +1,3 @@
-from rdkit.Chem import Draw
-from IPython.display import SVG, display
-import matplotlib.pyplot as plt
-from io import BytesIO
-from PIL import Image
-import cairosvg
-
 import sys
 import os
 
@@ -13,73 +6,28 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from evomol.representation import Molecule, SMILES, MolecularGraph
+from evomol.visualization.draw_mol import (
+    mol_to_svg,
+    draw_multiple_svgs_in_matplotlib,
+    draw_in_matplotlib,
+)
 
 Molecule.id_representation_class = SMILES
 Molecule.representations_class = [MolecularGraph]
 
 
-def mol_to_svg(molecule: Molecule, size=500, draw_index=True):
-    mol = molecule.get_representation(MolecularGraph).mol.GetMol()
+if __name__ == "__main__":
+    molecules = [Molecule("CCO"), Molecule("CCC"), Molecule("CCN")]
 
-    d = Draw.rdMolDraw2D.MolDraw2DSVG(size, size)
-    d.drawOptions().addStereoAnnotation = True
-    if draw_index:
-        d.drawOptions().addAtomIndices = True
-    d.DrawMolecule(mol)
-    d.FinishDrawing()
+    note_on_atoms = {0: "A", 1: "B", 2: "C"}
+    note_on_bonds = {0: "bond A", 1: "bond B"}
+    svg_1 = mol_to_svg(
+        molecules[0],
+        notes_on_atoms=note_on_atoms,
+        notes_on_bonds=note_on_bonds,
+    )
 
-    svg = d.GetDrawingText()
+    draw_in_matplotlib(svg_1)
 
-    return svg
-
-
-def draw_in_jupyter(svg: str) -> None:
-    display(SVG(svg))
-
-
-def draw_in_matplotlib(svg: str) -> None:
-    # convert SVG to PNG using cairosvg
-    png_data = cairosvg.svg2png(bytestring=svg.encode("utf-8"))
-
-    # load the PNG image with PIL
-    image = Image.open(BytesIO(png_data))
-
-    # create a matplotlib figure and display the image
-    fig, ax = plt.subplots(figsize=(image.width / 100, image.height / 100))
-    ax.imshow(image)
-    ax.axis("off")
-    plt.show()
-
-
-def draw_multiple_svgs_in_matplotlib(svgs: list) -> None:
-    # convert each SVG to PNG using cairosvg
-    png_datas = [
-        Image.open(BytesIO(cairosvg.svg2png(bytestring=svg.encode("utf-8"))))
-        for svg in svgs
-    ]
-
-    # set the number of columns and rows for the subplots
-    num_images = len(png_datas)
-    # 3 columns maximum
-    cols = min(3, num_images)
-    rows = (num_images + cols - 1) // cols
-
-    # create a figure with subplots
-    fig, axes = plt.subplots(rows, cols, figsize=(cols * 5, rows * 5))
-
-    # display each image in a subplot
-    for i, (image, ax) in enumerate(zip(png_datas, axes.flat)):
-        ax.imshow(image)
-        ax.axis("off")
-
-    # remove the axis for the empty subplots
-    for j in range(i + 1, len(axes.flat)):
-        axes.flat[j].axis("off")
-
-    plt.tight_layout()
-    plt.show()
-
-
-molecules = [Molecule("CCO"), Molecule("CCC"), Molecule("CCN")]
-svgs = [mol_to_svg(mol) for mol in molecules]
-draw_multiple_svgs_in_matplotlib(svgs)
+    svgs = [mol_to_svg(mol) for mol in molecules]
+    draw_multiple_svgs_in_matplotlib(svgs)
